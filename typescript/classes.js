1a480bd8c73d760778fe174d1827ae1567e191f4
Nathan Dantas <than_dantas@hotmail.com>
2021-07-12 22:53:12 -0300

    aprendendo sobre classes e interface no typescript

--- typescript/classes.js
+++ typescript/classes.js
@@ -18,3 +18,44 @@ var data = new Data(1, 2, 2020);
 console.log(data.dia);
 var data2 = new Data(1, 1);
 console.log(data.ano);
+
+var Carro = /** @class */ (function () {
+    function Carro(marca, modelo, velocidadeMaxima) {
+        if (velocidadeMaxima === void 0) { velocidadeMaxima = 220; }
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    Carro.prototype.alterarVelocidade = function (delta) {
+        if (delta < 0) {
+            if (this.velocidadeAtual + delta <= this.velocidadeMaxima) {
+                this.velocidadeAtual += delta;
+                console.log("O carro est\u00E1 andando a " + this.velocidadeAtual + "km/h");
+            }
+        }
+        else {
+            if (this.velocidadeAtual - delta >= 0) {
+                this.velocidadeAtual -= delta;
+                console.log("O carro est\u00E1 andando a " + this.velocidadeAtual + "km/h");
+                if (this.velocidadeAtual == 0) {
+                    console.log('Carro está parado');
+                }
+            }
+        }
+    };
+    Carro.prototype.acelerar = function () {
+        this.alterarVelocidade(5);
+    };
+    Carro.prototype.frear = function () {
+        this.alterarVelocidade(-5);
+    };
+    return Carro;
+}());
+var carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelerar();
+carro.acelerar();
+carro.acelerar();
+carro.acelerar();
+carro.frear();
+carro.acelerar();
--- typescript/classes.ts
+++ typescript/classes.ts
@@ -15,3 +15,68 @@ console.log(data.dia);
 
 const data2 = new Data(1,1);
 console.log(data.ano);
+
+class Carro {
+
+    private velocidadeAtual: number = 0;
+
+    constructor(
+        public marca: string,
+        public modelo: string,
+        private velocidadeMaxima: number = 220
+    ){}
+
+     private alterarVelocidade(delta: number){
+         const novaVelocidade = this.velocidadeAtual + delta;
+        if (delta < 0){
+            if (novaVelocidade >= this.velocidadeMaxima){
+                this.velocidadeAtual += delta;
+                console.log(`O carro está andando a ${this.velocidadeAtual}km/h`)
+            }
+        } else {
+            if (this.velocidadeAtual - delta >= 0){
+                this.velocidadeAtual -= delta;
+                console.log(`O carro está andando a ${this.velocidadeAtual}km/h`)
+                if(this.velocidadeAtual == 0){
+                    console.log('Carro está parado')
+                }
+            }
+        }
+    } 
+
+    acelerar(){
+       this.alterarVelocidade(5)
+    }
+
+    frear(){
+        this.alterarVelocidade(-5)
+    } 
+
+}
+
+class Camaro extends Carro{
+
+    private turbo = false;
+    
+    constructor(){
+        super('Chevrolet', 'Camaro', 500);
+    }
+
+    ligarTurbo(){
+        this.turbo = true;
+    }
+}
+
+const carro = new Carro ('Chevrolet', 'Prisma', 250);
+carro.acelerar();
+carro.acelerar();
+carro.acelerar();
+carro.acelerar();
+carro.frear();
+carro.acelerar();
+
+const camaro = new Camaro();
+
+camaro.acelerar()
+camaro.frear();
+camaro.ligarTurbo();
