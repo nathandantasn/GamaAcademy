94f18237cd4211fe1619270610908c8bc2c935e2
Nathan Dantas <than_dantas@hotmail.com>
2021-07-13 20:08:44 -0300

    modificando código de pesquisa de livros

--- javascript2/projeto/app.js
+++ javascript2/projeto/app.js
@@ -1,24 +1,40 @@
 const livros = require('./database.js')
 
 // pegar o input
-// se for simm, mostra as categorias disponíveis, pergunta qual categoria ela escolhe
+// se for sim, mostra as categorias disponíveis, pergunta qual categoria ela escolhe
 // se não, mostra todos os livros em ordem crescente pela quantidade de páginas
 
 const redline = require('readline-sync')
 
 const entradaInicial = redline.question('Deseja buscar um livro? S/N ')
 
-if (entradaInicial.toLocaleUpperCase() === 'S'){
-    console.log('Essas são as categorias disponíveis: ')
-    console.log ('Produtividade e estilo de vida', '/ História brasileira', '/ Américas', 
-    '/ Tecnologia', '/ Estilo de vida', '/ Autoavaliação', '/ Comportamento organizacional')
-    
-    const entradaCategoria = redline.question('Qual categoria você escolhe:')
+while (entradaInicial.toLocaleUpperCase() === 'S'){
 
-    const retorno = livros.filter(livros => livros.categoria === entradaCategoria)
-    console.table(retorno)
-} else {
+    if (entradaInicial.toLocaleUpperCase() === 'S'){
+        console.log('Busque por categoria, nome do autor ou nome do livro: ')
+    
+        const entrada = redline.question('Digite aqui o filtro de busca: ')
+
+        if(entrada.toLocaleLowerCase() === 'categoria'){
+            const entrada2 = redline.question('Digite a categoria do livro: ')
+            const retorno = livros.filter(livros => livros.categoria === entrada2);
+            console.table(retorno)
+        } else if(entrada.toLocaleLowerCase() === 'autor'){
+            let entrada2 = redline.question('Digite o nome do autor do livro: ')
+            const retorno = livros.filter(livros => livros.autor === entrada2);
+            console.table(retorno)
+        } else if(entrada.toLocaleLowerCase() === 'nome'){
+            const entrada2 = redline.question('Digite o nome do livro: ')
+            const retorno = livros.filter(livros => livros.nome === entrada2);
+            console.table(retorno)
+        } else {
+            console.log('Não temos este livro, por favor, tente buscar novamente')
+    }
+        entradaInicial = redline.question('Deseja buscar outro livro? S/N ')
+    } else {
     const livrosOrdenados =  livros.sort((a,b) => a.paginas - b.paginas)
     console.log('Esses são todos os livros dispníveis')
     console.table(livrosOrdenados)
+    entradaInicial = redline.question('Deseja buscar outro livro? S/N ')
+    }
 }
