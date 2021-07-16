3e9969ff97727827f4b1eed46d1c0d2170d08a3b
Nathan Dantas <than_dantas@hotmail.com>
2021-07-16 14:56:33 -0300

    usando renderização condicional para tratar o erro

--- react/gamagit/src/pages/Home/index.js
+++ react/gamagit/src/pages/Home/index.js
@@ -6,6 +6,7 @@ import { useHistory } from 'react-router-dom';
 function App(props) {
   const history = useHistory();
   const [usuario, setUsuario] = useState('');
+  const [erro, setErro] = useState(false);
   function handlePesquisa(){
     axios.get(`https://api.github.com/users/${usuario}/repos`).then(response =>{
       const repositories = response.data;
@@ -14,14 +15,21 @@ function App(props) {
         repositoriesName.push(repository.name);
       });
     localStorage.setItem('repositoriesName', JSON.stringify(repositoriesName));
+    setErro(false);
     history.push('/repositories');
+  })
+  .catch(err =>{
+    setErro(true);
   });
   }
   return (
-    <S.Container>
+    <S.HomeContainer>
+    <S.Content>
     <S.Input  className="usuarioInput" value={usuario} placeholder="Usuário" onChange={e => setUsuario(e.target.value)}/>
     <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
-    </S.Container>
+    </S.Content>
+    { erro ? <S.ErrorMsg>Ocorreu um erro. Tente novamente</S.ErrorMsg>: ' '}
+    </S.HomeContainer>
   );
 }
 
--- react/gamagit/src/pages/Home/styled.js
+++ react/gamagit/src/pages/Home/styled.js
@@ -1,8 +1,16 @@
 import styled from 'styled-components';
 
-export const Container = styled.div `
-width: 100vw;
+export const HomeContainer = styled.div `
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
 height: 100vh;
+`;
+
+export const Content = styled.div `
+width: 100vw;
+
 display: flex;
 align-items: center;
 justify-content: center;
@@ -34,3 +42,11 @@ border-radius: 0 .25rem .25rem 0;
     box-shadow: none;
 }
 `;
+
+export const ErrorMsg = styled.span `
+display: block;
+font-size: 0.65rem;
+color: red;
+font-weight: 600;
+margin-top: 1rem;
+`
