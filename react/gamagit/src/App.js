38cfd7f83ced781d7bb29ad3c7a3d7a59d4816f6
Nathan Dantas <than_dantas@hotmail.com>
2021-07-15 21:07:01 -0300

    adicionando estado ao usuario e aprendendo sobre api axios

--- react/gamagit/src/App.js
+++ react/gamagit/src/App.js
@@ -1,12 +1,15 @@
 import React, {useState} from "react";
+import axios from 'axios';
 
 function App(props) {
-  const [usuario, setUsuario] = useState('Nathan')
+  const [usuario, setUsuario] = useState('');
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response))
+  }
   return (
     <>
-    <p>{usuario}</p>
-    <input name="usuario" id="usuario" className="usuarioInput" placeholder="Usuário"/>
-    <button type="button">Pesquisar</button>
+    <input  className="usuarioInput" value={usuario} placeholder="Usuário" onChange={e => setUsuario(e.target.value)}/>
+    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
     </>
   );
 }
