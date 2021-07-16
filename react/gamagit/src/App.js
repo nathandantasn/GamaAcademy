e5a95fab7d70eff021fffd84f3f654971335a176
Nathan Dantas <than_dantas@hotmail.com>
2021-07-15 23:26:48 -0300

    adicionando estilos para as rotas e separando o site em componentes

--- react/gamagit/src/App.js
+++ react/gamagit/src/App.js
@@ -1,16 +1,10 @@
-import React, {useState} from "react";
-import axios from 'axios';
+import React from "react";
+import Routes from './routes';
+import './style.css';
 
-function App(props) {
-  const [usuario, setUsuario] = useState('');
-  function handlePesquisa(){
-    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response))
-  }
+function App() {
   return (
-    <>
-    <input  className="usuarioInput" value={usuario} placeholder="Usuário" onChange={e => setUsuario(e.target.value)}/>
-    <button type="button" onClick={handlePesquisa}>Pesquisar</button>
-    </>
+    <Routes/>
   );
 }
 
---
+++ react/gamagit/src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import axios from 'axios';
+import * as S from './styled';
+
+function App(props) {
+  const [usuario, setUsuario] = useState('');
+  function handlePesquisa(){
+    axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response))
+  }
+  return (
+    <S.Container>
+    <S.Input  className="usuarioInput" value={usuario} placeholder="Usuário" onChange={e => setUsuario(e.target.value)}/>
+    <S.Button type="button" onClick={handlePesquisa}>Pesquisar</S.Button>
+    </S.Container>
+  );
+}
+
+export default App;
