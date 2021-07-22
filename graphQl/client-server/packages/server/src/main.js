f71672bb588f88ff99ca487134e8453041dd3ce3
Nathan Dantas <than_dantas@hotmail.com>
2021-07-21 22:48:42 -0300

    subindo arquivos de modificação da aplicação

--- graphQl/client-server/packages/server/src/main.js
+++ graphQl/client-server/packages/server/src/main.js
@@ -1,6 +1,5 @@
-let createServer,read,readFile,resolve;_eed‍.w("http",[["createServer",["createServer"],function(v){createServer=v}]]);_eed‍.w("fs",[["read",["read"],function(v){read=v}],["readFile",["readFile"],function(v){readFile=v}]]);_eed‍.w("path",[["resolve",["resolve"],function(v){resolve=v}]]);
-
-
+import { createServer } from 'http';
+import { parse } from 'querystring';
 
 const server = createServer((request, response) =>{
     switch(request.url){
@@ -14,18 +13,17 @@ const server = createServer((request, response) =>{
             response.end();
             break;
         }
-        case '/sign-in': {
-            const path = resolve(__dirname, './pages/sign-in.html');
-            readFile(path, (error, file) => {
-                if(error){
-                    response.writeHead(500, "Can't process HTML file.");
-                    response.end();
-                    return;
-                }
-                response.writeHead(200);
-                response.write(file);
+        
+        case '/authenticate': {
+            let data = '';
+            request.on('data', (chunk) =>{
+                data += chunk;
+            });
+            request.on('end', () =>{
+                const params = parse(data);
                 response.end();
-            })
+            });
+            break;
         }
         default: {
             response.writeHead(404, 'Service not found.');
@@ -36,6 +34,7 @@ const server = createServer((request, response) =>{
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
 server.listen(PORT, HOSTNAME, () => {
     console.log(`Server is listening at http://${HOSTNAME}: ${PORT}`);
 });
